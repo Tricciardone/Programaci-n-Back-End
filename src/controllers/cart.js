e2d7e1877f5e10027dec79114f8455a590d1df22
Tomas Ricciardone <Ricciardonetomas@gmail.com>
2024-07-27 15:57:34 -0300

    entrega 3

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,62 +1,69 @@
-const {createCartDTO} = require('../dto/cart/cartDto') // aca no lo voy a usar
+// Importar servicios relacionados con el carrito
 const {
     addToCartService,
     getCartService,
     deleteOneService,
     buyCartService
-} = require('../services/cartServices')
+} = require('../services/cartServices');
 
+// Función para añadir al carrito
 async function addToCart(req, res, next) {
-    if(req.emailUserExist) {
-        let responseCart = await addToCartService(req.body.email, req.prod) 
-        res.json(responseCart)
-    } else {   
-        res.json('user-doesnt-exist')
-    }
-}
-
-async function getCart(req, res, next) {
-    if(req.emailUserExist) {
-        let responseGet = await getCartService(req.body.email)
-        res.json(responseGet) 
-    } else {   
-        res.json('user-doesnt-exist')
-    }
-}
-
-async function deleteOneCart(req, res, next) {
-    if(req.emailUserExist) {
-        let responseDeleteOne = await deleteOneService(req.body.email, req.prod)
-        res.json(responseDeleteOne)
-    } else {   
-        res.json('user-doesnt-exist')
-    }
-}
-
-async function buyCart(req, res, next) {
-    if(req.emailUserExist) {
-        let responseBuy = await buyCartService(req.body.email)
-        res.json(responseBuy)
+    if (req.emailUserExist) {
+        let responseCart = await addToCartService(req.body.email, req.prod);
+        res.json(responseCart);
     } else {
-        res.json('user-doesnt-exist')
+        res.json('user-doesnt-exist');
     }
 }
 
+// Función para obtener el carrito
+async function getCart(req, res, next) {
+    if (req.emailUserExist) {
+        let responseGet = await getCartService(req.body.email);
+        res.json(responseGet);
+    } else {
+        res.json('user-doesnt-exist');
+    }
+}
+
+// Función para eliminar un producto del carrito
+async function deleteOneCart(req, res, next) {
+    if (req.emailUserExist) {
+        let responseDeleteOne = await deleteOneService(req.body.email, req.prod);
+        res.json(responseDeleteOne);
+    } else {
+        res.json('user-doesnt-exist');
+    }
+}
+
+// Función para comprar el carrito
+async function buyCart(req, res, next) {
+    if (req.emailUserExist) {
+        let responseBuy = await buyCartService(req.body.email);
+        res.json(responseBuy);
+    } else {
+        res.json('user-doesnt-exist');
+    }
+}
+
+// Middleware para parsear parámetros de carrito
 function parseParamsCart(req, res, next) {
-    req.body.email = req.params.emailUser  
-    next()
+    req.body.email = req.params.emailUser;
+    next();
 }
 
+// Middleware para parsear parámetros de producto
 function parseParamsCartProduct(req, res, next) {
-    req.prod = req.params.idProduct
-    next()
+    req.prod = req.params.idProduct;
+    next();
 }
 
+// Exportar las funciones y middlewares
 module.exports = {
-    addToCart, 
-    getCart, 
+    addToCart,
+    getCart,
     deleteOneCart,
     parseParamsCart,
     parseParamsCartProduct,
     buyCart
-}
+};
